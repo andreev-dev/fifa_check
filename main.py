0de324ee807e5c82a5d9743c8f7977834efcb0d8
artem <art.andreev@gmail.com>
2018-06-17 12:15:09 +0300

    Получение и обработка данных с сайта fifa

---
+++ main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+
+f = open(r'data.json', "wb")
+ufr = requests.get("https://tickets.fifa.com/API/WCachedL1/ru/BasicCodes/GetBasicCodesAvailavilityDemmand?currencyId=USD")
+f.write(ufr.content)
+f.close()
+
+with open('data.json') as data_file:
+    data_item = json.loads(data_file.read())
+
+    for i in data_item['Data']['Availability']:
+        if i['a'] == 1 and i['c'] in (17, 56) and i['p'] in ('IMT20', 'IMT23', 'IMT30', 'IMT42', 'IMT50', 'IMT51', 'IMT52', 'IMT56', 'IMT57', 'IMT58', 'IMT62', 'IMT64'):
+
+            if i['c'] == 17:
+                cat = 'CAT 4'
+            elif i['c'] == 56:
+                cat = 'OV'
+
+            if i['p'] == 'IMT20':
+                match = '20.06 21:00, Kazan, Iran - Spain'
+            elif i['p'] == 'IMT23':
+                match = '21.06 21:00, Nizhny Novgorod, Argentina - Horvatia'
+            elif i['p'] == 'IMT30':
+                match = '24.06 15:00, Nizhny Novgorod, England - Panama'
+            elif i['p'] == 'IMT42':
+                match = '27.06 21:00, Nizhny Novgorod, Schveizaria - Kosta-Rica'
+            elif i['p'] == 'IMT50':
+                match = '30.06 17:00, Kazan'
+            elif i['p'] == 'IMT51':
+                match = '01.07 17:00, Moscow'
+            elif i['p'] == 'IMT52':
+                match = '01.07 21:00, Nizhny Novgorod'
+            elif i['p'] == 'IMT56':
+                match = '03.07 21:00, Moscow'
+            elif i['p'] == 'IMT57':
+                match = '06.07 17:00, Nizhny Novgorod'
+            elif i['p'] == 'IMT58':
+                match = '06.07 21:00, Kazan'
+            elif i['p'] == 'IMT62':
+                match = '11.07 21:00, Moscow'
+            elif i['p'] == 'IMT64':
+                match = '15.07 18:00, Moscow'
+
+            print(match, cat)
